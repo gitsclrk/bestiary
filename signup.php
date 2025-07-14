b0d1267f4fba1b84dbadc6a549bd562699bed3ee
Julia Pillora <juliaalyssa.pillora@wvsu.edu.ph>
2025-07-14 13:41:42 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -33,7 +33,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Request Access – Camp Valhalla</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 <body class="auth-body">
